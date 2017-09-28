7c76fa11c73d8cefd883908cdcc3f1ac17879074
Chris <chris.seq@hotmail.com>
2017-09-28 14:04:16 +1300

    offline database creation - chris

---
+++ RedFrogs/RedFrogs/RedFrogs.iOS/FileHelper.cs
@@ -0,0 +1,24 @@
+using RedFrogs.iOS;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace RedFrogs.iOS
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+            if (!Directory.Exists(libFolder))
+            {
+                Directory.CreateDirectory(libFolder);
+            }
+
+            return Path.Combine(libFolder, filename);
+        }
+    }
+}
